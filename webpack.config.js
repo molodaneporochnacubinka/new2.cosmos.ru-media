15deb2b19d3e209903949cb8758e533713d39a30
anouk <konstantinova.anouk@gmail.com>
2019-06-18 10:20:45 +0300

    Webpack-config2 commit

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+      favicon: './src/favicon.ico',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
